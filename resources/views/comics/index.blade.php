bc2fe995aaad92d02b6c048982b009da389443a8
SimoneBonvecchio84 <simonebonvecchio@gmail.com>
2024-06-17 15:15:51 +0200

    Fix: style form add th and td for action

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -4,36 +4,43 @@
     <div class="container">
         <h1>Lista Fumetti</h1>
         <table class="table">
-            <div>
-              <a class="btn btn-success" href="{{ route('comics.create') }}">Inserisci un nuovo fumetto</a>
+            <div class="py-4">
+                <a class="btn btn-success" href="{{ route('comics.create') }}">Inserisci un nuovo fumetto</a>
             </div>
             <thead>
-              <tr>
-                <th scope="col">Id</th>
-                <th scope="col">Titolo</th>
-                <th scope="col">Prezzo</th>
-                <th scope="col">Serie</th>
-              </tr>
+                <tr>
+                    <th scope="col">Id</th>
+                    <th scope="col">Titolo</th>
+                    <th scope="col">Prezzo</th>
+                    <th scope="col">Serie</th>
+                    <th scope="col">Dettagli</th>
+                    <th scope="col">Modifica</th>
+                    <th scope="col">Cancella</th>
+                </tr>
             </thead>
             <tbody>
                 @foreach ($comicsArray as $curComic)
-                <tr>
-                  <th scope="row">{{ $curComic->id }}</th>
-                  <td>{{ $curComic->title }}</td>
-                  <td>{{ $curComic->price }}</td>
-                  <td>{{ $curComic->series }}</td>
-                  <td class="d-flex gap-3" >
-                    <a class="btn btn-success" href="{{ route('comics.show',[ $curComic->id]) }}">Dettagli</a>
-                    <a class="btn btn-warning" href="{{ route('comics.edit',[ $curComic->id]) }}">Modifica</a>
-                    <form action="{{ route('comics.destroy',  [$curComic->id]) }}" method="POST">
-                      @csrf
-                      @method('DELETE')
-                      <button class="btn btn-danger">CANCELLA</button>
-                    </form>
-                  </td>
-                </tr>                    
+                    <tr>
+                        <th scope="row">{{ $curComic->id }}</th>
+                        <td>{{ $curComic->title }}</td>
+                        <td>{{ $curComic->price }}</td>
+                        <td>{{ $curComic->series }}</td>
+                        <td class="d-flex gap-3">
+                            <a class="btn btn-success" href="{{ route('comics.show', [$curComic->id]) }}">Dettagli</a>
+                        </td>
+                        <td>
+                            <a class="btn btn-warning" href="{{ route('comics.edit', [$curComic->id]) }}">Modifica</a>
+                        </td>
+                        <td>
+                            <form action="{{ route('comics.destroy', [$curComic->id]) }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button class="btn btn-danger">CANCELLA</button>
+                            </form>
+                        </td>                       
+                    </tr>
                 @endforeach
             </tbody>
-          </table>
+        </table>
     </div>
 @endsection
