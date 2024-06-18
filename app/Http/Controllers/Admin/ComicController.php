c67134a9a1eb9281144a53d7b91758c835320274
SimoneBonvecchio84 <simonebonvecchio@gmail.com>
2024-06-18 15:22:42 +0200

    Feat: add controls to form in create.balde

--- app/Http/Controllers/Admin/ComicController.php
+++ app/Http/Controllers/Admin/ComicController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Models\Comic;
+use App\Http\Requests\StoreComicRequest;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -28,7 +29,7 @@ class ComicController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreComicRequest $request)
     {
         $data = $request->all();
         $comic = new Comic();
--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -3,37 +3,50 @@
 @section('content')
     <div class="container py-5">
         <h1 class="py-3">Inserisci un nuovo Fumetto</h1>
+
+        @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error )
+                   <li>{{ $error }}</li> 
+                @endforeach
+            </ul>
+        </div> 
+        @endif
+
         <form action="{{ route('comics.store') }}" method="POST">
             {{-- Cookie per riconoscere il form al server --}}
             @csrf
 
             <div class="mb-3">
                 <label for="title" class="form-label">Titolo</label>
-                <input type="text" class="form-control" id="title" name="title">
+                <input value="{{ old('title') }}" type="text" class="form-control" id="title" name="title">
             </div>
             <div class="mb-3">
                 <label for="description" class="form-label">Descrizione</label>
-                <textarea class="form-control" name="description" id="description" rows="3"></textarea>
+                <textarea class="form-control" name="description" id="description" rows="3">
+                    {{ old('description') }}
+                </textarea>
             </div>
             <div class="mb-3">
                 <label for="thumb" class="form-label">Poster</label>
-                <input type="text" class="form-control" id="thumb" name="thumb">
+                <input value="{{ old('thumb') }}" type="text" class="form-control" id="thumb" name="thumb">
             </div>
             <div class="mb-3">
                 <label for="price" class="form-label">Prezzo</label>
-                <input type="number" class="form-control" id="price" name="price">
+                <input value="{{ old('price') }}" type="number" class="form-control" id="price" name="price">
             </div>
             <div class="mb-3">
                 <label for="series" class="form-label">Serie</label>
-                <input type="text" class="form-control" id="series" name="series">
+                <input value="{{ old('series') }}" type="text" class="form-control" id="series" name="series">
             </div>
             <div class="mb-3">
                 <label for="sale_date" class="form-label">Riedizione del</label>
-                <input type="text" class="form-control" id="sale_date" name="sale_date">
+                <input value="{{ old('sale_date') }}" type="text" class="form-control" id="sale_date" name="sale_date">
             </div>
             <div class="mb-3">
                 <label for="type" class="form-label">Tipologia</label>
-                <input type="text" class="form-control" id="type" name="type">
+                <input value="{{ old('type') }}"  type="text" class="form-control" id="type" name="type">
             </div>
             <div class="py-3">
                 <button type="submit" class="btn btn-primary">Salva</button>
